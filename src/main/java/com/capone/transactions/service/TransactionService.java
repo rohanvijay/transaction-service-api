3018b300e61233279a20b557f629b06e923d3d4a
Rohan Vijayvargiya <Rohan@Rohans-MacBook-Pro.local>
2018-09-19 00:33:34 -0500

    Creating seperate detailed and compact response transaction objects

--- src/main/java/com/capone/transactions/service/TransactionService.java
+++ src/main/java/com/capone/transactions/service/TransactionService.java
@@ -1,5 +1,6 @@
 package com.capone.transactions.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.capone.transactions.dao.impl.TransactionDAOImpl;
 import com.capone.transactions.exceptions.BadRequestException;
+import com.capone.transactions.model.DetailedTransaction;
 import com.capone.transactions.model.Transaction;
 import com.capone.transactions.model.TransactionSearchRequest;
 import com.capone.transactions.utils.EncryptionDecryptionUtility;
@@ -24,38 +26,44 @@ public class TransactionService {
 	@Autowired
 	TransactionsUtility transactionsUtility;
 
-	public Transaction getTransactionById(String transactionReferenceId) {
+	public DetailedTransaction getTransactionById(String transactionReferenceId) {
 
 		String transactionId = null;
 
 		try {
-			transactionId = encryptionDecryptionUtility
-					.decrypt(transactionReferenceId);
+			transactionId = encryptionDecryptionUtility.decrypt(transactionReferenceId);
 		} catch (Exception ex) {
 			throw new BadRequestException();
 		}
 
-		return transactionDAO.getTransaction(transactionId);
+		DetailedTransaction detailedTransactionFromBackend = transactionDAO.getTransaction(transactionId);
+		
+		return createResponseDetailedTransaction(detailedTransactionFromBackend);
 	}
 
 	public List<Transaction> getTransactionBySearchCriteria(
 			TransactionSearchRequest transactionRequest) {
 
 		String accountNumber = null;
-
+		List<Transaction> compactTransactionList = new ArrayList();
 		try {
-			accountNumber = encryptionDecryptionUtility
-					.decrypt(transactionRequest.getAccountReferenceId());
+			accountNumber = encryptionDecryptionUtility.decrypt(transactionRequest.getAccountReferenceId());
 		} catch (Exception ex) {
-
+			throw new BadRequestException();
 		}
-
 		transactionRequest.setAccountNumber(accountNumber);
-		return transactionDAO.getTransactionsList(transactionRequest);
+		
+		List<DetailedTransaction> detailedTransactionListFromBackend = transactionDAO.getTransactionsList(transactionRequest);
+		
+		for(int i=0; i<detailedTransactionListFromBackend.size();i++){
+			compactTransactionList.add(createResponseCompactTransaction(detailedTransactionListFromBackend.get(i)));
+		}
+		
+		return compactTransactionList;
 	}
 
 	public void addTransaction(String accountReferenceId,
-			Transaction transaction) {
+			DetailedTransaction transaction) {
 
 		String accountNumber = null;
 
@@ -68,10 +76,59 @@ public class TransactionService {
 
 		String transactionId = transactionsUtility.generateKey();
 
+		transaction.setCardReferenceId(accountNumber);
 		transaction.setTransactionReferenceId(transactionId);
 
 		transactionDAO.addToTransactionsList(accountNumber, transaction,
 				transactionId);
+	}
+	
+	private Transaction createResponseCompactTransaction(
+			DetailedTransaction detailedTransactionFromBackend) {
+		
+		Transaction transaction = new Transaction();
+		
+		transaction.setDebitCreditCode(detailedTransactionFromBackend.getDebitCreditCode());
+		transaction.setMerchantName(detailedTransactionFromBackend.getMerchantDetails().getMerchantName());
+		transaction.setTransactionAmount(detailedTransactionFromBackend.getTransactionAmount());
+		transaction.setTransactionDate(detailedTransactionFromBackend.getTransactionDate());
+		
+		try{
+		transaction.setCardReferenceId(encryptionDecryptionUtility.encrypt(detailedTransactionFromBackend.getCardReferenceId()));
+		transaction.setTransactionReferenceId(encryptionDecryptionUtility.encrypt(detailedTransactionFromBackend.getTransactionReferenceId()));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return transaction;
+	}
+
+	private DetailedTransaction createResponseDetailedTransaction(
+			DetailedTransaction detailedTransactionFromBackend) {
+		
+		DetailedTransaction responseTransaction=null;
+		
+		if(detailedTransactionFromBackend!=null){
+		responseTransaction= new DetailedTransaction();
+		
+		responseTransaction.setCardExpirationDate(detailedTransactionFromBackend.getCardExpirationDate());
+		responseTransaction.setCurrencyCode(detailedTransactionFromBackend.getCurrencyCode());
+		responseTransaction.setDebitCreditCode(detailedTransactionFromBackend.getDebitCreditCode());
+		responseTransaction.setPointOfSaleCardUsageCode(detailedTransactionFromBackend.getPointOfSaleCardUsageCode());
+		responseTransaction.setPointOfSalePresenceDescription(detailedTransactionFromBackend.getPointOfSalePresenceDescription());
+		responseTransaction.setPointOfSalePresenceCode(detailedTransactionFromBackend.getPointOfSalePresenceCode());
+		responseTransaction.setPostedDate(detailedTransactionFromBackend.getPostedDate());
+		responseTransaction.setTransactionAmount(detailedTransactionFromBackend.getTransactionAmount());
+		responseTransaction.setTransactionDate(detailedTransactionFromBackend.getTransactionDate());
+		responseTransaction.setTransactionDescription(detailedTransactionFromBackend.getTransactionDescription());
+		responseTransaction.setMerchantDetails(detailedTransactionFromBackend.getMerchantDetails());
+		try{
+			responseTransaction.setCardReferenceId(encryptionDecryptionUtility.encrypt(detailedTransactionFromBackend.getCardReferenceId()));
+			responseTransaction.setTransactionReferenceId(encryptionDecryptionUtility.encrypt(detailedTransactionFromBackend.getTransactionReferenceId()));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		}
+		return responseTransaction;
 	}
 
 }
